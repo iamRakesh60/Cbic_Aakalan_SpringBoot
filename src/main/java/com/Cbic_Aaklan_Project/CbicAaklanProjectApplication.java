9376c9882b7f5ca961385af9567d8c1104faa778
rakeshshit <iamrakeshit60@gmail.com>
2025-01-07 17:35:29 +0530

    update for making war file

--- src/main/java/com/Cbic_Aaklan_Project/CbicAaklanProjectApplication.java
+++ src/main/java/com/Cbic_Aaklan_Project/CbicAaklanProjectApplication.java
@@ -2,12 +2,18 @@ package com.Cbic_Aaklan_Project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CbicAaklanProjectApplication {
+public class CbicAaklanProjectApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CbicAaklanProjectApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CbicAaklanProjectApplication.class, args);
 	}
-
 }
